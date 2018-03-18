57ff56d876c634c8092dfd0e7f0031ab25fbafce
Paul Salaets <psalaets@gmail.com>
2018-03-18 12:47:30 -0400

    comment

--- test-transforms/test-helper.js
+++ test-transforms/test-helper.js
@@ -1,5 +1,14 @@
 const runInlineTest = require('jscodeshift/dist/testUtils').runInlineTest;
 
+/**
+ * Test a transform function.
+ *
+ * @param {Function} transform
+ * @param {String} path    - (fake) file path of source passed to transform
+ * @param {String} input   - Source code
+ * @param {String} output  - Expected transform result
+ * @param {Object} options - Options passed to transform, optional
+ */
 function testTransform(transform, path, input, output, options = {}) {
   const fileInfo = {
     path,
